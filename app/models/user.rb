e5b1edc621abe90f194dac68136129c4c95801a1
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-07-28 12:24:39 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         ASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :encrypted_password, with: PASSWORD_REGEX
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+          validates :first_name
+          validates :last_name
+          validates :first_name_kana
+          validates :last_name_kana
+        end
+        
+        validates :nickname, presence: true
+        validates :birthday, presence: true
+        
 end
