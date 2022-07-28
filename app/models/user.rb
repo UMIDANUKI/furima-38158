891b316e485d8a76a5212f40b866e57a9dc78862
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-07-28 15:51:08 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :encrypted_password, with: PASSWORD_REGEX
 
          with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+          validates :family_name 
           validates :first_name
-          validates :last_name
+          validates :family_name_kana
           validates :first_name_kana
-          validates :last_name_kana
         end
         
         validates :nickname, presence: true
         validates :birthday, presence: true
+        
+        validates_format_of :password, with: PASSWORD_REGEX
+
+        
+
+        
         
 end
