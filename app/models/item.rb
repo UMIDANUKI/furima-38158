917d29d43cf76b67e4c9146dc516ca44232a5e79
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-07-29 20:34:42 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,19 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :prefectures
   belongs_to :time_required
-  
+
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :image, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/},  numericality: { in: 300..9999999 }
+  
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefectures_id
+    validates :time_required_id
+  end
 end
