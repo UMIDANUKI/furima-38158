bcd07ef5787951522c1a78d26e2e2e145e927655
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-07-29 21:19:06 +0900

    コントローラ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,23 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:explanation,:image,:price,:category_id,:status_id,:delivery_charge_id,:prefectures_id,:time_required_id).merge(user_id: current_user.id)
+  end
   
 end
