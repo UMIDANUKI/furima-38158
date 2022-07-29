33777735faf9787294d54a39eb405ebb189076e9
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-07-29 18:59:27 +0900

    プルダウンリスト用ファイルを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefectures
+  belongs_to :time_required
+  
   has_one_attached :image
 end
