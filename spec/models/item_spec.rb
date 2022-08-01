f00a4ac4fc59937a1a198874999c48516c88a4ea
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-08-01 16:21:25 +0900

    商品出品機能の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -78,12 +78,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Time required must be other than 1")
       end
-      it '画像が添付されていないと出品できない' do
+      it 'userが紐づいていないと出品できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
-      it 'userが紐づいていないと出品できない' do
+      it '画像が添付されていないと出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
