6f711a25416b81a553fd2f1b55c7ec4eeaabd509
UMIDANUKI <108118254+UMIDANUKI@users.noreply.github.com>
2022-08-02 19:05:13 +0900

    コントローラを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :destroy]
-  before_action :find_item, only: [:show, :update]
+  before_action :move_to_index, only: [:edit]
+  before_action :find_item, only: [:show, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -33,10 +33,14 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.user == current_user
+      @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
